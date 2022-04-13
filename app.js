a41f0105273ae9bf44935856a220d16ec621e84e
MinhNV <minhnv@beetsoft.com.vn>
2022-04-13 17:40:38 +0700

    add question route

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.use(bodyParser());
 app.use(
   cors({
     origin: "*",
+    credentials: true
   })
 );
-require('dotenv').config()
 const port = process.env.PORT || 5000
 
 // !important!
@@ -18,6 +18,56 @@ const port = process.env.PORT || 5000
 app.get("/", (req, res) => {
   res.send("hello from simple server :)");
 });
+
+app.get("/api", (req, res) => {
+  res.status(400).send("hello from simple server :)");
+})
+
+app.post('/api/questions', (req, res) => {
+  console.log(req.body);
+  return res.json([
+    {
+      questionId: "1",
+      question: "this will be hide?",
+    },
+    {
+      questionId: '21234',
+      question: 'a question returned before this question is hidden because it is existed in the interview questions',
+    },
+    {
+      questionId: '3sdf',
+      question: 'erwer salary?'
+    },
+    {
+      questionId: '412',
+      question: 'What is RwerwfeactJS?'
+    },
+    {
+      questionId: '524',
+      question: 'What is sdfRedux?'
+    },
+    {
+      questionId: '623',
+      question: 'What is werJ1?'
+    },
+    {
+      questionId: '754',
+      question: 'How to increafwese performance?'
+    },
+    {
+      questionId: '83',
+      question: 'How to increweqase security?'
+    },
+    {
+      questionId: '9654',
+      question: 'explain about React lifecycle?'
+    },
+    {
+      questionId: '1340',
+      question: 'What is RefwwactJS?'
+    }
+  ])
+})
 
 app.get("/api/applicant/reference/init", (req, res) => {
   console.log("init");
